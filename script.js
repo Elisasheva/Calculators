e24c01346c5a512f07c40e1df4e8fa8310f43768
Kamila Brymora <brymora.kamila@gmail.com>
2020-08-04 07:51:29 +0200

    bmi

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let heightElement = document.querySelector(".js-height");
 let weightElement = document.querySelector(".js-weight");
 let formElement = document.querySelector(".js-form");
 let bmiElement = document.querySelector(".js-bmi");
+let resultElement = document.querySelector(".result");
+let buttonElement = document.querySelector("button");
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -12,6 +14,8 @@ formElement.addEventListener("submit", (event) => {
     let bmi = weight / ((height / 100) ** 2);
 
     bmiElement.innerText =  bmi.toFixed(2);
+    resultElement.classList.add("seen");
 
     console.log(bmi);
 });
+
